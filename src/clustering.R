5eb58dcc2297d1b561e639718b80f0ce133d584a
Dijkman <RDIJKMAN@campus.tue.nl>
2016-09-22 15:00:23 +0200

    Starting to add clustering.

---
+++ src/clustering.R
@@ -0,0 +1,16 @@
+library(fpc)
+
+test = cases.1$proctime
+
+testsample = sample(test, 1000)
+
+k = pamk(testsample)$nc
+
+# K-Means Cluster Analysis
+fit <- kmeans(test, k)
+
+# get cluster means 
+aggregate(test,by=list(fit$cluster),FUN=mean)
+
+# append cluster assignment
+test <- data.frame(test, fit$cluster)
